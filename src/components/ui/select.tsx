1aa582844153c7bec2226fcd577011672d83bc00
prashantwct <prasarsenal@gmail.com>
2025-06-27 22:52:19 +0530

    fix: remove invalid position prop from SelectPrimitive.Content in SelectContent

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -43,7 +43,6 @@ const SelectContent = React.forwardRef<
           'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
         className
       )}
-      position={position}
       {...props}
     >
       <SelectPrimitive.Viewport
